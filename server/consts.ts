e6849af93ea3ec7cccb1a58b0fe2293b035e004c
IsaacSteadman <isaac.steadman@yahoo.com>
2019-07-11 19:42:51 -0500

    Changed server config to configure PORT, CORS, and DEBUG

--- server/consts.ts
+++ server/consts.ts
@@ -3,6 +3,21 @@ import { readFileSync } from "fs";
 
 export const SERVER_DATA_LOCATION = resolve(__dirname, '../server-data');
 export const serverConfig = JSON.parse(readFileSync(resolve(__dirname, '../config.json'), 'utf8'));
-export const PORT = 3050;
-export const CORS = false;
-export const DEBUG = true;
+export const PORT = (
+  (
+    Object.prototype.hasOwnProperty.call(serverConfig, 'Port')
+    && typeof serverConfig.Port === 'number')
+  ? serverConfig.Port : 3050
+);
+export const CORS = (
+  (
+    Object.prototype.hasOwnProperty.call(serverConfig, 'Cors')
+    && typeof serverConfig.Cors === 'boolean')
+  ? serverConfig.Cors : false
+);
+export const DEBUG = (
+  (
+    Object.prototype.hasOwnProperty.call(serverConfig, 'Debug')
+    && typeof serverConfig.Debug === 'boolean')
+  ? serverConfig.Debug : true
+);
